cf7cf25eed1f749100ca3455389d9030d74174b5
lorran10 <lorrandesolsa@gmail.com>
2022-10-20 10:39:48 -0300

    comit 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.css';
+
+
+export function Header(){
+return(
+    <header className={styles.header}>
+    <strong>Ignite Feed</strong>
+    </header>
+ );
+}
+
+
+
+
+
+
